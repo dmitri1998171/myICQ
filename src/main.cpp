20ebe57724e2049f9108b35f41f42d9c9a915340
dmitri1998171 <dmitri1998171@icloud.com>
2021-05-03 22:48:31 +0700

    add output text in the block in the output area

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,14 @@
 Mutex mutex;
 
 struct GraphicsThread {
-    Font font;
+    Font* font;
+    Text* recv_text;
     TcpSocket* socket;
     RenderWindow* window;
+    RectangleShape* output_rect;
+    RectangleShape* output_text_rect;
     float output_rect_pos;
-}graph_thread_struct;
+}thread_struct;
 
 struct networkStruct {
     String message;
@@ -23,15 +26,12 @@ void create_rect(RectangleShape *rect, float width, float height, float x, float
     rect->setPosition(Vector2f(x, y));
 }
 
-void text_params_func(Font* font, Text* text, String message, float y) {
-    font->loadFromFile("./media/fonts/CyrilicOld.TTF");
-   
-    message = "Enter a message...";
+void text_params_func(Font* font, Text* text, String message, Color color, float x, float y) {
     text->setFont(*font);
     text->setCharacterSize(14);
     text->setString(message);
-    text->setFillColor(Color(128, 128, 128, 100));
-    text->setPosition(Vector2f(10, y));
+    text->setFillColor(Color(color));
+    text->setPosition(Vector2f(x, y));
 }
 
 int main() {
@@ -40,18 +40,18 @@ int main() {
     float output_rect_pos = HEIGHT * 0.8;
     float input_rect_pos = HEIGHT - output_rect_pos;
     float sidebar_width = 400;
-    char title[100] = {"myICQ    Username: "};
-    
+    char title[100] = {"myICQ    Name: "};
+
     json_parser_create(&settings_struct);
 
     strcat(title, settings_struct.username);
     RenderWindow window(VideoMode(WIDTH, HEIGHT), title);
-    // window.setPosition(Vector2i(VideoMode::width));
 
     /* Draw background */
     RectangleShape output_rect;
     RectangleShape input_rect;
     RectangleShape side_rect;
+    RectangleShape output_text_rect;
 
     create_rect(&output_rect, WIDTH - sidebar_width, HEIGHT, sidebar_width, 0);
     create_rect(&input_rect, (WIDTH - sidebar_width) - 20, input_rect_pos - 20, sidebar_width + 10, output_rect_pos + 10);
@@ -61,10 +61,13 @@ int main() {
     side_rect.setFillColor(Color(39, 40, 49));
 
     /* Fonts and texts */
-    Font font;
+    String message = "Enter a message...";
     Text text;
-    String message;
-    text_params_func(&font, &text, message, output_rect_pos + 5);
+    Text recv_text;
+    Font font;
+    font.loadFromFile("./media/fonts/CyrilicOld.TTF");
+    Color text_color(128, 128, 128, 100);
+    text_params_func(&font, &text, message, text_color, input_rect.getGlobalBounds().left + 10, input_rect.getGlobalBounds().top + 5);
     
     /* Network */
     TcpSocket socket;
@@ -72,10 +75,13 @@ int main() {
     
     /* Create a thread */
     // window.setActive(false);
-    graph_thread_struct.font = font;
-    graph_thread_struct.socket = &socket;
-    graph_thread_struct.window = &window;
-    graph_thread_struct.output_rect_pos = output_rect_pos;
+    thread_struct.font = &font;
+    thread_struct.recv_text = &recv_text;
+    thread_struct.socket = &socket;
+    thread_struct.window = &window;
+    thread_struct.output_rect = &output_rect;
+    thread_struct.output_text_rect = &output_text_rect;
+    thread_struct.output_rect_pos = output_rect_pos;
 
     Thread thread(&output_thread_func);
     thread.launch();
@@ -107,6 +113,7 @@ int main() {
                     side_rect.setSize(Vector2f(0, 0));
                     create_rect(&output_rect, WIDTH, HEIGHT, 0, 0);
                     create_rect(&input_rect, WIDTH - 20, input_rect_pos - 20, 10, output_rect_pos + 10);
+                    text.setPosition(Vector2f(input_rect.getGlobalBounds().left + 10, input_rect.getGlobalBounds().top + 5));
                 }
                 if(winX > sub_min_x) {
                     sidebar_width = 400;
@@ -114,12 +121,13 @@ int main() {
                     create_rect(&output_rect, WIDTH - sidebar_width, HEIGHT, sidebar_width, 0);
                     create_rect(&input_rect, (WIDTH - sidebar_width) - 20, input_rect_pos - 20, sidebar_width + 10, output_rect_pos + 10);
                     create_rect(&side_rect, sidebar_width, HEIGHT, 0, 0);
+                    text.setPosition(Vector2f(input_rect.getGlobalBounds().left + 10, input_rect.getGlobalBounds().top + 5));
                 }
             }
 
             if (Mouse::isButtonPressed(Mouse::Left)) {
-                Vector2i localPosition = Mouse::getPosition(window);
-                if(localPosition.y > output_rect_pos) {
+                Vector2i mouse_pos = Mouse::getPosition(window);
+                if(mouse_pos.y > input_rect.getGlobalBounds().top && mouse_pos.x > input_rect.getGlobalBounds().left) {
                     write_flag = 1;
                     message = "";
                     text.setString(message);
@@ -155,29 +163,43 @@ int main() {
                 }
             }
         }  
-        
+
         window.clear();
         window.draw(side_rect);
         window.draw(output_rect);
         window.draw(input_rect);
+        window.draw(output_text_rect);
         window.draw(text);
+        window.draw(recv_text);
         window.display();
     }
     return 0;
 }
 
 void output_thread_func() {
+    int sep = 10;
+    float output_rect_x, output_rect_y, output_rect_w;
+    float str_length = 50;
+    float font_size = 14;
+    float string_count = 1;
     size_t received;
 
-    // graph_thread_struct.window->setActive(true);
-    while (graph_thread_struct.window->isOpen()) {
-        if (graph_thread_struct.socket->receive(&net_struct, sizeof(net_struct), received) != Socket::Done)
+    // thread_struct.window->setActive(true);
+    while (thread_struct.window->isOpen()) {
+        if (thread_struct.socket->receive(&net_struct, sizeof(net_struct), received) != Socket::Done)
             die_With_Error(DEVICE, "Failed to receive a message from the server!");
 
-        // Text recv_text;
-        // recv_text.setFont(graph_thread_struct.font);
-
         cout << "Received: \"" << net_struct.message.toAnsiString() << "\" (" << received << " bytes)" << endl;
+
+        output_rect_x = thread_struct.output_rect->getGlobalBounds().left + 10;
+        output_rect_y = thread_struct.output_rect->getGlobalBounds().top;
+        output_rect_w = font_size * string_count;
+
+        create_rect(thread_struct.output_text_rect, str_length, output_rect_w + 5, output_rect_x, output_rect_y + sep);
+        thread_struct.output_text_rect->setFillColor(Color(34, 52, 79));
+        text_params_func(thread_struct.font, thread_struct.recv_text, net_struct.message, Color::Black, output_rect_x + 5, output_rect_y + sep);
+        
+        sep += output_rect_w + 10;
         sleep(milliseconds(10));
     }
 }
